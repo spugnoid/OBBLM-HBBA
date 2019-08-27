60186ec2460d109cb8fed64c86e356addc6f25b8
spugnoid <spugnoid@gmail.com>
2019-08-26 18:06:39 -0700

    Update mysql_upgrade_queries.php
    Create seasons played 'splayed' and wants to retire 'w2retire' columns in players table

--- lib/mysql_upgrade_queries.php
+++ lib/mysql_upgrade_queries.php
@@ -5,6 +5,10 @@
 */
 $upgradeSQLs = array(
     // future upgrade versions should be "to" version and should be integers, rather than "from-to".
+    102 => array(
+    SQLUpgrade::runIfColumnNOTExists('players', 'splayed', 'ALTER TABLE players ADD COLUMN splayed TINYINT SIGNED'),
+        SQLUpgrade::runIfColumnNOTExists('players', 'w2retire', 'ALTER TABLE players ADD COLUMN w2retire BOOLEAN DEFAULT FALSE'),
+    )
     101 => array(
         SQLUpgrade::updateDatabaseVersion(101)
     ),
